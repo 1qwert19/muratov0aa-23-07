3837cc159e88caca394efdfe4aff309f9cf64d7e
1qwert19 <129896852+1qwert19@users.noreply.github.com>
2024-10-08 20:54:24 +0300

    Add files via upload

---
+++ muratov0.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+
+    char inputChar;
+    std::cout << "Введите символ: ";
+    std::cin >> inputChar;
+
+    std::cout << "Вы ввели символ: " << inputChar << std::endl;
+
+    return 0;
+}
